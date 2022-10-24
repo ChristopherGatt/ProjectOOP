05d26ecd1d5206be84fb0b24260912b59872a133
ChristopherGatt <chris.gatt.3@gmail.com>
2022-10-24 21:17:33 +1100

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const inquirer = require("inquirer");
+
+const teamArr = [];
+
+function appMenu() {
+  function createManager() {
+    // inquirer.prompt(manager info)
+    //.then(instaniate a new Manager(name, id, email, officenum)
+    //  teamArr.push(manager)
+    // createTeam())
+  }
+  function createTeam() {
+    //inquire prompt list switch case Engineer, case Intern, case no more memebers buildteam()
+  }
+  function addEngineer() {}
+  function addIntern() {}
+  function buildTeam() {
+    // generate HTML(teamArr) fs.writeFile -> dist/index.html
+  }
+
+  createManager();
+}
+
+appMenu();
---
+++ script.js
@@ -0,0 +1,43 @@
+const team = [
+  {
+    name: "Marc",
+    role: "Team Leader",
+    id: 0,
+    email: "marc@wizardsmail.com",
+    github: "https://github.com/mssandeepkamath",
+  },
+
+  {
+    name: "Hannah",
+    role: "Engineer",
+    id: 1,
+    email: "hanzfryingpan@noodle.com",
+    github: "https://github.com/mssandeepkamath",
+  },
+
+  {
+    name: "Jack",
+    role: "Game Master",
+    id: 2,
+    email: "slayinnoobs@mordor.com",
+    github: "https://github.com/mssandeepkamath",
+  },
+
+  {
+    name: "Lily",
+    role: "Security",
+    id: 3,
+    email: "lillily@ili.com",
+    github: "https://github.com/mssandeepkamath",
+  },
+
+  {
+    name: "Tony",
+    role: "Engineer",
+    id: 4,
+    email: "tonyyont@hotmailz.com",
+    github: "https://github.com/mssandeepkamath",
+  },
+];
+
+console.log(team);
